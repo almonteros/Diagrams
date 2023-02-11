2a09b83df523e87bd0b8f617375b12bf4b511ee1
Alessandro Monteros <70610930+almonteros@users.noreply.github.com>
2023-02-10 17:48:47 -0800

    Add initial files

---
+++ target/debug/deps/diagrams_rust.d
@@ -0,0 +1,5 @@
+D:\OneDrive\Desktop\Rust\diagrams_rust\target\debug\deps\diagrams_rust.exe: src\main.rs
+
+D:\OneDrive\Desktop\Rust\diagrams_rust\target\debug\deps\diagrams_rust.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/diagrams_rust.d
@@ -0,0 +1,1 @@
+D:\OneDrive\Desktop\Rust\diagrams_rust\target\debug\diagrams_rust.exe: D:\OneDrive\Desktop\Rust\diagrams_rust\src\main.rs
